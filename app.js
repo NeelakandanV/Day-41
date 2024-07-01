bb716b600be56241f605834ed261d50a5c72a263
NeelakandanV <hari0709venkatesan@gmail.com>
2024-07-01 17:51:26 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -13,7 +12,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var cors = require('cors');
 var router = express.Router();
 const { MongoClient } = require('mongodb');
 const {dbUrl} = require('../Database/config')
+
+app.use(cors());
 
 // Welcome Page
 router.get('/', async(req, res)=>{
