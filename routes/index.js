8fd8dc76511f683a5352617cbdaf133579aa43f9
NeelakandanV <hari0709venkatesan@gmail.com>
2024-07-01 17:07:35 +0530

    Third commit

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,12 @@ const { MongoClient } = require('mongodb');
 const {dbUrl} = require('../Database/config')
 
 app.use(express.json())
-app.use(cors())
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // Welcome Page
 router.get('/', async(req, res)=>{
